07ec119428634aa33a240deb00916f55c0acbdd6
EdenIvgi <edenavgi@gmail.com>
2025-08-19 17:50:42 +0300

    add pagination

--- pages/TodoIndex.jsx
+++ pages/TodoIndex.jsx
@@ -35,34 +35,41 @@ export function TodoIndex() {
             })
             .catch(() => showErrorMsg('Had trouble removing the todo'))
     }
-
     function onToggleTodo(todo) {
         const todoToSave = { ...todo, isDone: !todo.isDone }
-        todoService.save(todoToSave)
-            .then((savedTodo) => {
-                setTodos(prevTodos => prevTodos.map(currTodo => (currTodo._id !== todo._id) ? currTodo : { ...savedTodo }))
-                showSuccessMsg(`Todo is ${(savedTodo.isDone)? 'done' : 'back on your list'}`)
+        saveTodo(todoToSave)
+            .then(() => {
+                showSuccessMsg(`Updated ${todoToSave.txt} successfully`)
+                if (todoToSave.isDone) {
+                    return changeBalance(10)
+                }
             })
-            .catch(err => {
-                console.log('err:', err)
-                showErrorMsg('Cannot toggle todo ' + todoId)
-            })
+            .catch(() => showErrorMsg('Had trouble updating the todo'))
     }
 
-    if (!todos) return <div>Loading...</div>
+      function onSetFilterSort(filterSort) {
+        setFilterBy(_filterBy => ({ ..._filterBy, ...filterSort }))
+    }
+
+
+    function onChangePageIdx(diff) {
+        let newPageIdx = +filterBy.pageIdx + diff
+        if (newPageIdx < 0) newPageIdx = maxPage - 1
+        if (newPageIdx >= maxPage) newPageIdx = 0
+        onSetFilterSort({ ...filterBy, pageIdx: newPageIdx, })
+    }
+
     return (
         <section className="todo-index">
-            <TodoFilter filterBy={filterBy} onSetFilterBy={setFilterBy} />
-            <div>
-                <Link to="/todo/edit" className="btn" >Add Todo</Link>
-            </div>
-            <h2>Todos List</h2>
-            <TodoList todos={todos} onRemoveTodo={onRemoveTodo} onToggleTodo={onToggleTodo} />
-            <hr />
-            <h2>Todos Table</h2>
-            <div style={{ width: '60%', margin: 'auto' }}>
-                <DataTable todos={todos} onRemoveTodo={onRemoveTodo} />
-            </div>
+            <TodoFilter filterBy={filterBy} onSetFilterBy={onSetFilterSort} />
+            <TodoSort filterBy={filterBy} onSetFilterBy={onSetFilterSort} />
+            <PaginationBtns filterSortBy={filterBy} onChangePageIdx={onChangePageIdx} />
+            <Link to="/todo/edit" className="add-todo-btn btn" >Add Todo</Link>
+            {isLoading
+                ? <h1 className="loader">Loading...</h1>
+                : <TodoList todos={todos} onRemoveTodo={onRemoveTodo} onToggleTodo={onToggleTodo} />
+            }
+
         </section>
     )
 }
