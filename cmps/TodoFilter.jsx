0f6cfd0deeeb48bb4765344beaa58b84db435661
EdenIvgi <edenavgi@gmail.com>
2025-08-19 18:05:04 +0300

    improve filtering

--- cmps/TodoFilter.jsx
+++ cmps/TodoFilter.jsx
@@ -1,55 +1,79 @@
-const { useState, useEffect } = React
+import { utilService } from '../services/util.service.js'
+
+const { useState, useEffect, useRef } = React
 
 export function TodoFilter({ filterBy, onSetFilterBy }) {
+  const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
+  const debouncedSetFilterRef = useRef(utilService.debounce(onSetFilterBy, 500))
 
-    const [filterByToEdit, setFilterByToEdit] = useState({...filterBy})
+  useEffect(() => {
+    debouncedSetFilterRef.current(filterByToEdit)
+  }, [filterByToEdit])
 
-    useEffect(() => {
-        // Notify parent
-        onSetFilterBy(filterByToEdit)
-    }, [filterByToEdit])
+  function handleChange({ target }) {
+    const field = target.name
+    let value = target.value
 
-    function handleChange({ target }) {
-        const field = target.name
-        let value = target.value
+    switch (target.type) {
+      case 'number':
+      case 'range':
+        value = +value || ''
+        break
 
-        switch (target.type) {
-            case 'number':
-            case 'range':
-                value = +value || ''
-                break
+      case 'checkbox':
+        value = target.checked
+        break
 
-            case 'checkbox':
-                value = target.checked
-                break
-
-            default: break
-        }
-
-        setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
+      default:
+        break
     }
+    // onSetFilterBy({ [field]: value })
+    setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+  }
 
-    // Optional support for LAZY Filtering with a button
-    function onSubmitFilter(ev) {
-        ev.preventDefault()
-        onSetFilterBy(filterByToEdit)
-    }
+  // Optional support for LAZY Filtering with a button
+  function onSubmitFilter(ev) {
+    ev.preventDefault()
+    onSetFilterBy(filterByToEdit)
+  }
 
-    const { txt, importance } = filterByToEdit
-    return (
-        <section className="todo-filter">
-            <h2>Filter Todos</h2>
-            <form onSubmit={onSubmitFilter}>
-                <input value={txt} onChange={handleChange}
-                    type="search" placeholder="By Txt" id="txt" name="txt"
-                />
-                <label htmlFor="importance">Importance: </label>
-                <input value={importance} onChange={handleChange}
-                    type="number" placeholder="By Importance" id="importance" name="importance"
-                />
+  const { txt, importance, isDone } = filterByToEdit
 
-                <button hidden>Set Filter</button>
-            </form>
-        </section>
-    )
+  return (
+    <section className="todo-filter">
+      <h2>Filter Todos</h2>
+      <form onSubmit={onSubmitFilter}>
+        <select
+          value={isDone}
+          className="flex justify-center align-center"
+          name="isDone"
+          onChange={(ev) => handleChange(ev)}
+        >
+          <option value="all">All</option>
+          <option value="done">Done</option>
+          <option value="undone">Active</option>
+        </select>
+
+        <input
+          value={txt}
+          onChange={handleChange}
+          type="search"
+          placeholder="By Txt"
+          id="txt"
+          name="txt"
+        />
+        <label htmlFor="importance">Importance: </label>
+        <input
+          value={importance}
+          onChange={handleChange}
+          type="number"
+          placeholder="By Importance"
+          id="importance"
+          name="importance"
+        />
+
+        <button>Set Filter</button>
+      </form>
+    </section>
+  )
 }
